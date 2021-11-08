7427fce36fa2dec945230bbd4c619a069189ed9e
bilcon <bilcon@swbell.net>
2021-11-08 14:03:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-const generatePage = () => 'Name: Jane, Github: janehub';
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
 
-console.log(generatePage());
+console.log(generatePage('Jane, 'janehub'));
