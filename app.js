29ee03d2625cf020e06cc110a1ac1fd1be95fdac
bilcon <bilcon@swbell.net>
2021-11-08 14:56:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 const name = profileDataArgs[0];
 const github = profileDataArgs[1];
 
-console.log(generatePage(name, github));
+
 
 const generatePage = (userName, githubName) => {
   return `
