7cf8d4c0e103fd98ffb3f81db505f18cccc00e3a
bilcon <bilcon@swbell.net>
2021-11-09 05:56:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const generatePage = (name, github) => {
    `;
 };
 
-fs.writeFile('index.html", generatePage(name, github), err => {
+fs.writeFile('index.html', generatePage(name, github), err => {
   if (err) throw err;
 
 console.log('Portfolio complete! Check out index.html to see the output!');
