ed3c1109771005761825c395746471cee25d1935
bilcon <bilcon@swbell.net>
2021-11-09 05:33:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const generatePage = () => 'Name; Jane, Github: janehub';
- console.log(generatePage());
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+ if (err) throw err;
+ 
+ console.log('Portfolio complete! Check out index.html to see the output!');
+});
