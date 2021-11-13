f166df60def98ad4bb1c83a5abf30c10d322bc64
Bill Morris <bilcon@swbell.net>
2021-11-13 10:20:50 -0600

    template updated for sections

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html', pageHTML, err => {
-  //if (err) throw err;
-
-  //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
 const promptUser = () => {
-
   return inquirer.prompt([
     {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name? (Required)',
-    validate: nameInput => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log('Please enter your name!');
-        return false;
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
       }
-    }
     },
     {
       type: 'input',
@@ -137,6 +129,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
+
 
